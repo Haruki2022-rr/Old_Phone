9b1f8603b78932dbd36f4873e200486b32bf0f13
hham0997 <hham0997@uni.sydney.edu.au>
2025-05-04 19:15:15 +1000

    add UserSchema.pre to hash password in  backend/models/User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname:  { type: String, required: true },
-  email:     { type: String, required: true },
+  email:     { type: String, required: true, unique: true},
   password:  { type: String, required: true }, 
 });
+
+// before document.save(), hash the password
+UserSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 10);
+  });
 
 module.exports = mongoose.model('User', UserSchema, 'users');
