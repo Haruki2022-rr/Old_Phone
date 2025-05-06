9fdce768a6f02b96d9bff40a17f2640c9ed4f863
hham0997 <hham0997@uni.sydney.edu.au>
2025-05-06 18:54:00 +1000

    added routes for signup and email verification

--- backend/routes/oldPhoneDeals.routes.js
+++ backend/routes/oldPhoneDeals.routes.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
+const express = require("express");
 // use require to get controller here
+const { signup, emailVerification } = require("../controllers/auth");
 
 // add method to use controller function here
 // example: 
 // GET    /api/oldPhoneDeals/auth
-// router.get("/auth", sighup);
+
+//signup : /api/oldPhoneDeals/auth/signup
+router.post("/auth/signup", signup);
+
+//email verification : /api/oldPhoneDeals/auth/verifyemail/:token
+router.get("/auth/verifyemail/:token", emailVerification);
+
 
 
 
