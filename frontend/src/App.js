7fcc00191158e0980bc1a4ec624af449ebfc4ec1
hham0997 <hham0997@uni.sydney.edu.au>
2025-05-08 22:44:23 +1000

    minor change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Main   from "./pages/Main";     
+// import {AuthPage, ResetPasswordPage}  from "./pages/Auth";
+// import Checkout from "./pages/Checkout";
+// import Profile from "./pages/Profile";
+// import "./pages/tailwind.css";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Main />} />          
+//           <Route path="auth" element={<AuthPage />} />
+//           <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+//           <Route path="checkout" element={<Checkout />} />
+//           <Route path="profile" element={<Profile />} /> 
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Main   from "./pages/Main";     
 import {AuthPage, ResetPasswordPage}  from "./pages/Auth";
-import Checkout from "./pages/Checkout";
-import Profile from "./pages/Profile";
 import "./pages/tailwind.css";
 
 export default function App() {
@@ -14,8 +36,6 @@ export default function App() {
           <Route index element={<Main />} />          
           <Route path="auth" element={<AuthPage />} />
           <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="profile" element={<Profile />} /> 
         </Route>
       </Routes>
     </BrowserRouter>
