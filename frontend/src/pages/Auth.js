b1c3ecd8075fece2c8cc49cf91c61d501d9bf6f7
wqiu8445 <wqiu8445@uni.sydney.edu.au>
2025-05-08 18:02:55 +1000

    Whole website load without errors

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,10 @@
+const AuthPage = () => {
+    return (
+        <div>
+        <h1>Auth Page</h1>
+        <p>Welcome to the auth page!</p>
+        </div>
+    );
+}
+
+export default AuthPage;
---
+++ frontend/src/pages/Checkout.js
@@ -0,0 +1,10 @@
+const CheckoutPage = () => {
+    return (
+        <div>
+        <h1>Checkout Page</h1>
+        <p>Welcome to the checkout page!</p>
+        </div>
+    );
+}
+
+export default CheckoutPage;
