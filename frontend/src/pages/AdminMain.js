c471f933790d1fef369d5fd4e6a42b49763f0ebc
wqiu8445 <wqiu8445@uni.sydney.edu.au>
2025-05-13 01:53:00 +1000

    small ui change

--- frontend/src/pages/AdminMain.js
+++ frontend/src/pages/AdminMain.js
@@ -783,7 +783,7 @@ const AdminMain = () => {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded shadow-md w-full max-w-lg relative">
             <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl"
+              className="absolute top-3 right-4 text-gray-500 hover:text-gray-700 text-2xl"
               onClick={() => setSelectedReview(null)}
               aria-label="Close"
             >
